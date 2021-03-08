718c45710269ca784e662c6e34361b6f340becc3
lehongngoc <=>
2021-03-08 22:37:25 +0700

    exercise php Signed-off-by: lehongngoc <=>

---
+++ exercises/php/exercise6/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exercise6</title>
+</head>
+<body>
+	<?php
+	$day=5;
+
+	switch ($day) {
+		case 0:
+			echo "Thứ 2";
+			break;
+		case 1:
+			echo "Thứ 3";
+			break;
+		case 2:
+			echo "Thứ 4";
+			break;
+		case 3:
+			echo "Thứ 5";
+			break;
+		case 4:
+			echo "Thứ 6";
+			break;
+		case 5:
+			echo "Thứ 7";
+			break;
+		case 6:
+			echo "Chủ nhật";
+			break;
+		default:
+			echo "Không có ngày nào";
+			break;
+	}
+	?>
+</body>
+</html>
