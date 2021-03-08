e74a10635b2b6139c16b7e67d7404601f41d1903
lehongngoc <75593215+lehongngoc@users.noreply.github.com>
2021-03-08 22:44:31 +0700

    exercise php

---
+++ exercises/php/week1/exercise3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exercise1</title>
+</head>
+<body>
+<?php
+	$a="3";
+	$b="2";
+	echo ("tổng=".($a+$b)."<br>");
+	echo ("hiệu=".($a-$b)."<br>");
+	echo ("tích=".($a*$b)."<br>");
+	echo ("thương=".($a/$b)."<br>");
+	echo ("số chia phép dư=".($a%$b));
+?>
+</body>
+</html>
