7c756ab84d111132c0320482be98acce72a8c02d
lehongngoc <75593215+lehongngoc@users.noreply.github.com>
2021-03-09 15:40:41 +0700

    php exercise

---
+++ exercises/php/week1/lesson1/exercise2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exercise2</title>
+</head>
+<body>
+<?php
+	$a=5;
+	if($a%2==0){
+		echo "Là số chẵn";
+	}
+	else{
+		echo "Là số lẻ";
+	}
+?>
+</body>
+</html>
