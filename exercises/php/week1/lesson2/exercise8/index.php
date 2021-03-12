ee1569b2b99ac27ca1af24ea8cb89c338583f038
lehongngoc <75593215+lehongngoc@users.noreply.github.com>
2021-03-12 13:26:05 +0700

    php exercise

--- exercises/php/week1/lesson2/exercise8/index.php
+++ exercises/php/week1/lesson2/exercise8/index.php
@@ -1,32 +1,38 @@
-<!DOCTYPE html>
-<html>
+<!Doctype html>
+<html lang="en">
 <head>
-	<title>Bảng số nguyên tố</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>exercise8</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
-	<table width="320px">
-        <?php
-        for ($i = 0; $i < 10; $i++) {
-            echo "<tr";
-            for ($j = 1; $j <= 10; $j++) {
-                $count = 0;
-                for ($k = 2; $k <= sqrt($i * 10 + $j); $k++) {
-                    if (($i * 10 + $j) % $k == 0) {
-                        $count++;
-                    }
-                }
-                if ($i * 10 + $j < 2) {
-                    echo "<td>" . ($i * 10 + $j) . "</td>";
-                } else if ($count == 0) {
-                    echo "<td class='bg-succes'>" . ($i * 10 + $j) . "</td>";
-                } else {
-                    echo "<td>" . ($i * 10 + $j) . "</td>";
-                }
-            }
-            echo "</tr>";
+<table>
+    <?php
+       function snt(int $nummber){
+        if($nummber <2){
+            return false;
         }
-        ?>
-    </table>
+        $sq = sqrt($nummber);
+        for($i = 2; $i <= $sq ; $i++){
+            if ($nummber  %  $i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    $number = 1;
+    for ($i = 1; $i < 10; $i++) {
+        echo '<tr class="row">';
+        for ($j = 1; $j < 10; $j++) {
+            if(snt($number)){
+                echo '<td class="col bg-success">'.$number.'</div>';
+                }else{
+                    echo '<td class=" col">'.$number.'</div>';
+                }
+                $number++;
+            }
+        echo '</tr>';
+    }  
+?>
+</table>
 </body>
 </html>
