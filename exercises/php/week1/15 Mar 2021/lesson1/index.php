6a723ec6f3340ed2f963abb8976e2cc854f6346f
lehongngoc <75593215+lehongngoc@users.noreply.github.com>
2021-03-22 16:07:03 +0700

    push 15 Mar 2021

---
+++ exercises/php/week1/15 Mar 2021/lesson1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exercise1</title>
+</head>
+<body>
+	<form action="index1.php" method="post">
+		<h3>Login</h3>
+		<label>Username:</label>
+		<input type="text" name="name"><br><br>
+		<label>Password:</label>
+		<input type="Password" name="Password"><br><br>
+		<button type="submit" name="submit">Login</button>
+        <button type="reset">Reset</button>
+
+		
+	</form>
+
+</body>
+</html>
