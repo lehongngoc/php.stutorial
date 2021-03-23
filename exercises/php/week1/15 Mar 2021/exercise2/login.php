ed974ce665c285316ad05e67080245fecb49d5c7
lehongngoc <75593215+lehongngoc@users.noreply.github.com>
2021-03-23 15:07:58 +0700

    push 15 Mar 2021

--- exercises/php/week1/15 Mar 2021/exercise2/login.php
+++ exercises/php/week1/15 Mar 2021/exercise2/login.php
@@ -3,11 +3,11 @@ session_start();
 if (isset($_SESSION['name'])&&!empty($_SESSION['name'])){
     echo "login success"."<br>";
     echo "Your Name:".$_SESSION['name'];
-    echo "<a href='check-logout.php'>Logout</a>";
+    echo "<a href='logout.php'>Logout</a>";
 
 }
 else{
-    echo "Bạn cần đăng nhập để sử dụng chức năng này !";
+    echo "Bạn cần đăng nhập!";
     header('Refresh:2;url=index.php',true,303);
     session_unset();
     session_destroy();
