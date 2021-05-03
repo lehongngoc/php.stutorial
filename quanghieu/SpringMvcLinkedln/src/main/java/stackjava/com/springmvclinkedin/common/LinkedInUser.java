425a1780add8e0837bf0f986efe6a0578dc17c21
Dung Ta Van <itprono3@gmail.com>
2021-05-03 20:23:08 +0700

    update config

--- quanghieu/SpringMvcLinkedln/src/main/java/stackjava/com/springmvclinkedin/common/LinkedInUser.java
+++ quanghieu/SpringMvcLinkedln/src/main/java/stackjava/com/springmvclinkedin/common/LinkedInUser.java
@@ -1,6 +1,19 @@
 package stackjava.com.springmvclinkedin.common;
-public class LinkedInUser{
-  private String id;
-  private String name;
-  // getter - setter
+
+public class LinkedInUser {
+	private String id;
+	private String name;
+
+	public LinkedInUser(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
